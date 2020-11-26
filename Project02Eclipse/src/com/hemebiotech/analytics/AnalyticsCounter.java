f1b5984d035aae09a222e7121f024ee33eb47cf8
chrisignas <chrisignas@gmail.com>
2020-11-26 15:20:48 -0500

    Initial Notes

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,8 +11,13 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
+
+		/* Above - Author names the file but does not provide path or directory
+		for "symptoms.txt". Program does not know where to find "symptoms.txt".
+		This is the reason for the exception.
+		 */
 
 		int i = 0;	// set i to 0
 		int headCount = 0;	// counts headaches
@@ -24,10 +29,16 @@ public class AnalyticsCounter {
 				System.out.println("number of headaches: " + headCount);
 			}
 			else if (line.equals("rush")) {
+				/* "rush" should be "rash"
+				 */
 				rashCount++;
+				/* Author may have forgotten to print "number of rashes"
+				 */
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
+				/* Author may have forgotten to print "number of pupils"
+				 */
 			}
 
 			line = reader.readLine();	// get another symptom
@@ -35,6 +46,10 @@ public class AnalyticsCounter {
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
+
+		/* Above - No path given for "result.out"
+		 */
+
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
