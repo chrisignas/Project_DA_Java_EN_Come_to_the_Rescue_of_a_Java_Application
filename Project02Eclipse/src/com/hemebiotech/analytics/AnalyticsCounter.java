39a4ef3cd4c143ef219dd308b0ff6325a5604dd9
chrisignas <chrisignas@gmail.com>
2020-12-01 22:31:49 -0500

    Printed symptoms as a map in alphabetical order. Need to count frequencies of each symptom.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,43 +3,72 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
 	
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws Exception {
+
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-			}
+        Map<String, String> map = new TreeMap<String, String>();
+        String line = "";
+        while((line = reader.readLine())!=null){
+            map.put(getField(line),line);
+        }
+//        Map<String, Integer> map = new TreeMap<String, Integer>();
+//        for (String a : args) {
+//            Integer freq = map.get(a);
+//            map.put(a, (freq == null) ? 1 : freq + 1);
+//        }
+        reader.close();
 
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
+        // write output
+        FileWriter writer = new FileWriter("result1.out");
+        for(String val : map.values()){
+            writer.write(val);
+            writer.write('\n');
+        }
+        writer.close();
+    }
 
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    private static String getField(String line) {
+        return line.split(" ")[0];
+    }
+}
 
-			line = reader.readLine();	// get another symptom
-		}
+
+
+//		String line = reader.readLine();
+
+//		while (line != null) {
+//			System.out.println("symptom from file: " + line);
+//			if (line.equals("headache")) {
+//				headacheCount++;
+//			}
+//
+//			else if (line.equals("rash")) {
+//				rashCount++;
+//			}
+//
+//			else if (line.contains("pupils")) {
+//				pupilCount++;
+//			}
+//
+//			line = reader.readLine();	// get another symptom
+//		}
 		
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
-}
+//		FileWriter writer = new FileWriter ("result.out");
+//
+//		writer.write("headache: " + headacheCount + "\n");
+//		writer.write("rash: " + rashCount + "\n");
+//		writer.write("dialated pupils: " + pupilCount + "\n");
+//		writer.close();
+//	}
+//}
